80e754eecd9e88f4cc23def3c3b9bcbf1f6a2ecf
flashman90 <gilbertinus6@gmail.com>
2026-07-11 23:08:39 +0200

    Force long polling Firestore

--- src/firebase.js
+++ src/firebase.js
@@ -15,5 +15,5 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = initializeFirestore(app, {
-  experimentalAutoDetectLongPolling: true,
+  experimentalForceLongPolling: true,
 });
